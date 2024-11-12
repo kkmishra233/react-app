1bc29721537d1bf87f70db96a0efa9eb2ae96d46
Krishna kumar Mishra <47304900+kkmishra233@users.noreply.github.com>
2024-11-12 14:33:12 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,148 +1,106 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate
-} from "react-router-dom";
-import {
-  Button,
-  Collapse,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  Nav,
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  UncontrolledDropdown
-} from "reactstrap";
-import "./App.css";
-import Plugins from "./components/Plugins/Plugins";
-import Profile from "./components/Profile";
-import Deck from "./Job/Deck";
-import Secured from "./keycloak_login";
-import ErrorComponent from "./ErrorComponent";
-import SecPopUp from './components/SecPopUp/SecPopUp';
-import DirectionsBoatIcon from "@material-ui/icons/DirectionsBoat";
+import Keycloak from "keycloak-js";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
+import { Spinner } from "reactstrap";
+import keycloak_json_file from "./keycloak.json";
 
-class App extends Component {
+class Secured extends Component {
   constructor(props) {
     super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false,
-      isLoading: false,
-      isAuthed: false,
-      token: "",
-      name: "",
-      keycloak: null
-    };
-
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get("code");
-    if (code) {
-      this.getUserData(code);
-    }
-
-    this.logout = this.logout.bind(this);
+    this.state = { keycloak: null, authenticated: false };
   }
 
-  getUserData(code) {
-    let headers = { "Content-Type": "application/json" };
-    fetch(`/api/auth/git_login/?code=${code}`, { headers, method: "GET" }).then(res => {
-      if (res.status < 500) {
-        res.json().then(data => {
-          localStorage.setItem("token", data.token);
-          localStorage.setItem("username", data.user.username);
-          localStorage.setItem("name", data.user.name);
+  componentDidMount() {
+    const keycloakConfig = {
+      realm: keycloak_json_file["realm"],
+      url: keycloak_json_file["auth-server-url"],
+      clientId: keycloak_json_file["resource"],
+      secret: keycloak_json_file["credentials"]["secret"]
+    };
+
+    const keycloak = Keycloak({
+      realm: keycloakConfig.realm,
+      url: keycloakConfig.url,
+      "ssl-required": "none",
+      clientId: keycloakConfig.clientId,
+      credentials: {
+        secret: keycloakConfig.secret
+      },
+      "confidential-port": 0
+    });
+
+    keycloak
+      .init({ onLoad: "login-required", promiseType: "native" })
+      .then(authenticated => {
+        if (authenticated) {
+          localStorage.setItem("token", keycloak.token);
+          localStorage.setItem("keycloak", keycloak);
+          localStorage.setItem(
+            "name",
+            keycloak.tokenParsed.name || keycloak.tokenParsed.preferred_username
+          );
+          this.props.onLogin(
+            keycloak.token,
+            keycloak.tokenParsed.given_name,
+            keycloak
+          );
           this.setState({
-            isAuthed: true,
-            token: data.token
+            keycloak: keycloak,
+            authenticated: authenticated
           });
-        });
+        } else {
+          // Redirect or handle unauthenticated state
+          window.location.reload();
+        }
+      })
+      .catch(() => {
+        // Handle keycloak initialization error
+        console.error("Keycloak initialization failed");
+        window.location.reload();
+      });
+
+    axios.interceptors.request.use(
+      config => {
+        if (keycloak.token) {
+          return keycloak
+            .updateToken(8)
+            .then(() => {
+              config.headers.Authorization = "Bearer " + keycloak.token;
+              localStorage.setItem("token", keycloak.token);
+              return config;
+            })
+            .catch(() => {
+              // Handle login failure if token refresh fails
+              keycloak.login();
+              return Promise.reject("Keycloak token refresh failed");
+            });
+        }
+        return Promise.reject("No Keycloak token available");
+      },
+      error => {
+        // Handle request error
+        return Promise.reject(error);
       }
-    });
-  }
-
-  onLogin(token, name, keycloak) {
-    this.setState({
-      isAuthed: true,
-      token,
-      name,
-      keycloak
-    });
-  }
-
-  getAuth() {
-    const { isAuthed, token } = this.state;
-    return {
-      isAuthed,
-      token
-    };
-  }
-
-  PrivateRoute({ element: Element, ...rest }) {
-    const { isAuthed, isLoading } = this.state;
-    if (isLoading) {
-      return <em>Loading...</em>;
-    } else if (!isAuthed) {
-      const redirectTo = `/keycloak-login/${encodeURIComponent(window.location.pathname)}`;
-      return <Navigate to={redirectTo} />;
-    } else {
-      return <Element {...rest} />;
-    }
-  }
-
-  toggle() {
-    this.setState({ isOpen: !this.state.isOpen });
-  }
-
-  logout() {
-    localStorage.clear();
-    this.setState({ isAuthed: false, token: "", name: "", keycloak: null });
+    );
   }
 
   render() {
+    const { authenticated, keycloak } = this.state;
+    const redirectTo = this.props.match?.params?.id
+      ? decodeURIComponent(this.props.match.params.id)
+      : "/deck/ciconfig";
+
+    if (keycloak && authenticated) {
+      return <Navigate to={redirectTo} />;
+    }
+
     return (
-      <Router>
-        <div>
-          <Navbar color="light" light expand="md">
-            <NavbarBrand href="/">App Name</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <UncontrolledDropdown nav inNavbar>
-                  <DropdownToggle nav caret>
-                    Options
-                  </DropdownToggle>
-                  <DropdownMenu right>
-                    <DropdownItem>Profile</DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem onClick={this.logout}>Logout</DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-              </Nav>
-            </Collapse>
-          </Navbar>
-          <Routes>
-            <Route path="/" element={<Deck />} />
-            <Route path="/plugins" element={<Plugins />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/secured" element={<Secured />} />
-            <Route path="/error" element={<ErrorComponent />} />
-            <Route path="/popup" element={<SecPopUp />} />
-            <Route path="/keycloak-login/*" element={<DirectionsBoatIcon />} />
-            {/* Use the PrivateRoute component to protect routes */}
-            <Route
-              path="/protected"
-              element={<this.PrivateRoute element={Profile} />}
-            />
-          </Routes>
-        </div>
-      </Router>
+      <div style={{ padding: "15rem", textAlign: "center" }}>
+        <Spinner style={{ width: "5rem", height: "5rem", color: "#000053" }} />
+      </div>
     );
   }
 }
 
-export default App;
+export default Secured;
