385e713527578a2739945a1849144c87584cd864
Krishna kumar Mishra <47304900+kkmishra233@users.noreply.github.com>
2024-11-12 18:52:41 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,106 +1,30 @@
-import React, { Component } from "react";
-import Keycloak from "keycloak-js";
-import axios from "axios";
-import { Navigate } from "react-router-dom";
-import { Spinner } from "reactstrap";
-import keycloak_json_file from "./keycloak.json";
+import React from "react";
+import { Button } from "reactstrap";
+import { useNavigate } from "react-router-dom";
 
-class Secured extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { keycloak: null, authenticated: false };
-  }
+const ErrorComponent = () => {
+  const navigate = useNavigate(); // Use navigate hook
 
-  componentDidMount() {
-    const keycloakConfig = {
-      realm: keycloak_json_file["realm"],
-      url: keycloak_json_file["auth-server-url"],
-      clientId: keycloak_json_file["resource"],
-      secret: keycloak_json_file["credentials"]["secret"]
-    };
+  return (
+    <div className="container">
+      <center>
+        <h3>This page doesn't exist</h3>
+        <Button
+          style={{
+            background: "#000053",
+            margin: "10px 0px"
+          }}
+          onClick={() => {
+            if (window.location.href.includes("deck"))
+              navigate("/deck/ciconfig");
+            else navigate("/");
+          }}
+        >
+          Go to Homepage
+        </Button>
+      </center>
+    </div>
+  );
+};
 
-    const keycloak = Keycloak({
-      realm: keycloakConfig.realm,
-      url: keycloakConfig.url,
-      "ssl-required": "none",
-      clientId: keycloakConfig.clientId,
-      credentials: {
-        secret: keycloakConfig.secret
-      },
-      "confidential-port": 0
-    });
-
-    keycloak
-      .init({ onLoad: "login-required", promiseType: "native" })
-      .then(authenticated => {
-        if (authenticated) {
-          localStorage.setItem("token", keycloak.token);
-          localStorage.setItem("keycloak", keycloak);
-          localStorage.setItem(
-            "name",
-            keycloak.tokenParsed.name || keycloak.tokenParsed.preferred_username
-          );
-          this.props.onLogin(
-            keycloak.token,
-            keycloak.tokenParsed.given_name,
-            keycloak
-          );
-          this.setState({
-            keycloak: keycloak,
-            authenticated: authenticated
-          });
-        } else {
-          // Redirect or handle unauthenticated state
-          window.location.reload();
-        }
-      })
-      .catch(() => {
-        // Handle keycloak initialization error
-        console.error("Keycloak initialization failed");
-        window.location.reload();
-      });
-
-    axios.interceptors.request.use(
-      config => {
-        if (keycloak.token) {
-          return keycloak
-            .updateToken(8)
-            .then(() => {
-              config.headers.Authorization = "Bearer " + keycloak.token;
-              localStorage.setItem("token", keycloak.token);
-              return config;
-            })
-            .catch(() => {
-              // Handle login failure if token refresh fails
-              keycloak.login();
-              return Promise.reject("Keycloak token refresh failed");
-            });
-        }
-        return Promise.reject("No Keycloak token available");
-      },
-      error => {
-        // Handle request error
-        return Promise.reject(error);
-      }
-    );
-  }
-
-  render() {
-    const { authenticated, keycloak } = this.state;
-    const redirectTo = this.props.match?.params?.id
-      ? decodeURIComponent(this.props.match.params.id)
-      : "/deck/ciconfig";
-
-    if (keycloak && authenticated) {
-      return <Navigate to={redirectTo} />;
-    }
-
-    return (
-      <div style={{ padding: "15rem", textAlign: "center" }}>
-        <Spinner style={{ width: "5rem", height: "5rem", color: "#000053" }} />
-      </div>
-    );
-  }
-}
-
-export default Secured;
+export default ErrorComponent;
